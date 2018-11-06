59c338e8cc474bf3c70d6f04341b66a1f2567cae
Dmitry Stropalov <d.stropalov@gmail.com>
2018-11-06 23:02:39 +0100

    Currency model code clean up

--- src/main/java/com/openpf/budgetmanager/accounting/model/Currency.java
+++ src/main/java/com/openpf/budgetmanager/accounting/model/Currency.java
@@ -1,21 +1,11 @@
 package com.openpf.budgetmanager.accounting.model;
 
-//import javax.persistence.*;
 import org.springframework.data.annotation.Id;
 
-import java.util.List;
-
-//@Entity
-//@Table(name = "currency")
 public class Currency {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    //@Column(columnDefinition = "VARCHAR(3)", unique = true)
     public String code;
-
-    //@OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL, mappedBy="currency")
-    //public List<Account> accounts;
 }
