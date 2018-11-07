c2b3667f6cbb3e81d833b79c53db2997b6078e16
Dmitry Stropalov <d.stropalov@gmail.com>
2018-11-07 01:05:48 +0100

    Code clean up from comments

--- src/main/java/com/openpf/budgetmanager/ManagerApplication.java
+++ src/main/java/com/openpf/budgetmanager/ManagerApplication.java
@@ -2,10 +2,8 @@ package com.openpf.budgetmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@EnableJpaRepositories
 @SpringBootApplication
 @EnableTransactionManagement
 public class ManagerApplication {
