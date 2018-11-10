3368257f52d44ecc070868cedf14d72bf64b5502
Dmitry Stropalov <d.stropalov@gmail.com>
2018-11-10 19:14:20 +0100

    Fix small issues in Circle CI configuration and local run script

--- .circleci/run-local.sh
+++ .circleci/run-local.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+## Optionally commit hash could be added
+## --form revision=3fd40af037730db3e2288c2d8afbec9511cac325 \
+
 curl -v \
 --user ${CIRCLE_TOKEN}: \
 --request POST \
---form revision=3fd40af037730db3e2288c2d8afbec9511cac325 \
 --form config=@config.yml \
 --form notify=false \
 https://circleci.com/api/v1.1/project/github/h8/budget-manager/tree/develop
